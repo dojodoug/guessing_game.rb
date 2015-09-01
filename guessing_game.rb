45969a46c6c29bec7cc11e62946e12235506d8aa
Doug Bryant <douglas.e.bryant@gmail.com>
2015-09-01 17:18:10 -0400

    Add methods double_guess and toolow_toohigh

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,7 +1,21 @@
+def double_guess
+  array.include?(input)
+end
+
+def toolow_toohigh
+  if input < comp_guess && num_guesses < 5
+    puts "Your guess is too low. Try again."
+  elsif input > comp_guess && num_guesses < 5
+    puts "Your guess is too high. Try again."
+  else input == comp_guess && num_guesses < 5
+    puts "BIG Winner! Your guessing game is STRONG!"
+    break
+end
+
 
 input = 0
 num_guesses = 0
-array = Array.new
+array = []
 comp_guess = rand(1..100)
 
 puts "For reference only: Computer number is - - #{comp_guess}"
@@ -9,48 +23,19 @@ puts "I'm thinking of a number between 1 - 100. Try and guess the number. Double
 
 until num_guesses > 5
   input = gets.chomp.to_i
-  array << input
-  num_guesses += 1
 
-  if input == 0
+  if double_guess
+    puts "Deja vu...you have already selected that number."
+    puts "Try again."
+  elsif input == 0
     puts "Does not compute. Numbers only please. Start over."
     break
-  elsif input < comp_guess && num_guesses < 5
-    puts "Your guess is too low. Try again."
-  elsif input > comp_guess && num_guesses < 5
-    puts "Your guess is too high. Try again."
-  elsif input == comp_guess && num_guesses < 5
-    puts "Big Winner! Your guessing game is STRONG!"
-    break
+  elsif toolow_toohigh
   else
     puts "Sorry you lost..."
     break
   end
+
+  array << input
+  num_guesses += 1
 end
-
-=begin
-  if
-    puts "Please guess a number"
-    guesses = []
-    input = gets.chomp
-    guesses << input
-  else
-    puts "Sorry your game is now over."
-  end
-=end
-
-
-=begin
-puts "Please guess a number"
-guesses = []
-input = gets.chomp
-
-  while
-    if input != ''
-      puts "Does not compute. I need numbers!"
-    else
-    guesses.push input
-    input = gets.chomp
-    end
-  end
-=end
